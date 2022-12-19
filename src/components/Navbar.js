a7177afecd05e51723f9660899f89451f174d348
nwebb123 <68877910+nwebb123@users.noreply.github.com>
2022-12-19 18:45:21 -0500

    initial commit + navbar
    Implemented CRA and Navbar component.

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "../imgs/airbnb_1.png";
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <nav className="container">
+        <img className="navbar-logo" src={logo} alt="Airbnb logo" /> 
+    </nav>
+  );
+}
+
+export default Navbar;
