48c6defafce7f11f2de5014754677620e7247f17
nwebb123 <68877910+nwebb123@users.noreply.github.com>
2022-12-19 21:58:04 -0500

    update
    Implemented Card component and some styling, values are hard coded. Next step is implementing Cards Section and passing in properties to cards.

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,23 @@
 import React from 'react'
 import './Card.css';
+import exampleCardImage from "../imgs/katie-zaferes-swimmer.png";
+import star from "../imgs/star.png";
 
-function Card() {
+function Card(props) {
   return (
-    <div>Card</div>
+    <div className="card-container">
+        <img src={exampleCardImage} alt="" className='card-image' />
+
+        <div className="card-body">
+            <img src={star} alt="Review Star" /> 
+            <span>5.0</span>
+            <span className="gray">(6)</span>
+            <span className="gray">&#x2022; USA</span>
+            <p>Life Lessons with Katie Zaferes</p>
+            <p><b>From $136</b> / person</p>
+          
+        </div>
+    </div>
   )
 }
 
