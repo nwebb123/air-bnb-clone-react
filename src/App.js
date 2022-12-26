b489bf1cd1be1a4de7e755099b6f68104cce792c
nwebb123 <68877910+nwebb123@users.noreply.github.com>
2022-12-26 14:17:33 -0500

    update
    Completed mapping experiences into cards at bottom. Next is fixing styling for last card

--- src/App.js
+++ src/App.js
@@ -1,31 +1,41 @@
-
-import './App.css';
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import Card from './components/Card';
-
-import KatieFlores from "../src/imgs/katie-zaferes-swimmer.png";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Card from "./components/Card";
+import data from "../src/data";
 
 function App() {
+  const Experiences = data.map((exp) => {
+    return (
+      <Card
+        key={exp.id}
+        img={exp.coverImg}
+        rating={exp.stats.rating}
+        reviewCount={exp.stats.reviewCount}
+        location={exp.location}
+        title={exp.title}
+        price={exp.price}
+      />
+    );
+  });
+
   return (
     <>
-    <Navbar />
-    <Hero />
-    <Card 
+      <Navbar />
+      <Hero />
+      <section className="cards-list">{Experiences}</section>
+    </>
+  );
+}
+
+/* <Card 
       img={KatieFlores}
       rating={5.0}
       reviewCount={6}
       country="USA"
       title="Life Lessons with Katie Zaferes"
       price={136}
-
-      
-      
-      />
-    </>
-    
-
-  );
-}
+      /> 
+      */
 
 export default App;
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,21 +1,20 @@
 import React from 'react'
 import './Card.css';
-//import exampleCardImage from "../imgs/katie-zaferes-swimmer.png";
-// import KatieFlores from "../imgs/katie-zaferes-swimmer.png";
+
 import star from "../imgs/star.png";
-//{`../imgs/${props.img}`}
+
 function Card(props) {
   return (
     <div className="card-container">
-        <img src={props.img} alt="" className='card-image' />
+        <img src={`../imgs/${props.img}`} alt="" className='card-image' />
 
         <div className="card-body">
             <img src={star} alt="Review Star" /> 
             <span>{props.rating}</span>
             <span className="gray">({props.reviewCount})</span>
-            <span className="gray">&#x2022; {props.country}</span>
+            <span className="gray">&#x2022; {props.location}</span>
             <p>{props.title}</p>
-            <p><b>From ${props.price}</b> / person</p>
+            <p><b>From ${props.price}</b>/ person</p>
           
         </div>
     </div>
