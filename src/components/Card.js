a3431c220defe6a3774a749d9c24dadacf8f6e73
nwebb123 <68877910+nwebb123@users.noreply.github.com>
2022-12-26 15:13:10 -0500

    update
    fixed importing images and styling cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,24 +1,25 @@
-import React from 'react'
-import './Card.css';
+import React from "react";
+import "./Card.css";
 
 import star from "../imgs/star.png";
 
 function Card(props) {
   return (
     <div className="card-container">
-        <img src={`../imgs/${props.img}`} alt="" className='card-image' />
+      <img src={`../imgs/${props.img}`} alt="" className="card-image" />
 
-        <div className="card-body">
-            <img src={star} alt="Review Star" /> 
-            <span>{props.rating}</span>
-            <span className="gray">({props.reviewCount})</span>
-            <span className="gray">&#x2022; {props.location}</span>
-            <p>{props.title}</p>
-            <p><b>From ${props.price}</b>/ person</p>
-          
-        </div>
+      <div className="card-body">
+        <img src={star} alt="Review Star" className="card-star" />
+        <span>{props.rating}</span>
+        <span className="gray">({props.reviewCount})</span>
+        <span className="gray">&#x2022; {props.location}</span>
+      </div>
+      <p className="card-title">{props.title}</p>
+      <p className="card-price">
+        <b>From ${props.price}</b>/ person
+      </p>
     </div>
-  )
+  );
 }
 
-export default Card
+export default Card;
